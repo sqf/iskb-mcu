161b741af59d477e8fbe286d02db552a0146ca3d
Samir Senhadri <sqf@o2.pl>
2015-11-20 23:02:01 +0100

    Periodically reading from sensor and sending data.

--- testConnect.lua
+++ testConnect.lua
@@ -1,20 +1,27 @@
 wifi.setmode(wifi.STATION)
 wifi.sta.config("Illuminati","jarektalib666")
 gpio14 = 5
-status,temp,humi,temp_decimial,humi_decimial = dht.readxx(gpio14)
+
 print('aaaaaaaaaa')
 print(wifi.sta.status())
-if( status == dht.OK ) then
-  print("DHT Temperaturee:"..temp..";".."Humidity:"..humi)
-elseif( status == dht.ERROR_CHECKSUM ) then
-  print( "DHT Checksum error." );
-elseif( status == dht.ERROR_TIMEOUT ) then
-  print( "DHT Time out." );
+local readData = function() 
+    status,temp,humi,temp_decimial,humi_decimial = dht.readxx(gpio14)
+    if( status == dht.OK ) then
+      print("DHT Temperaturee:"..temp..";".."Humidity:"..humi)
+    elseif( status == dht.ERROR_CHECKSUM ) then
+    print( "DHT Checksum error." );
+    elseif( status == dht.ERROR_TIMEOUT ) then
+      print( "DHT Time out." );
+    end
 end
-conn=net.createConnection(net.TCP, 0) 
-conn:on("receive", function(conn, pl) print(pl) end)
---conn:on("sent", function(conn) numreq = numreq + 1 end)
-conn:on("connection", function(conn) conn:send("GET /temperaturaget?temperature="..temp.." HTTP/1.1\r\nHost: termometr.senhadri.pl\r\n"
-    .."Connection: close\r\nAccept: */*\r\n\r\n") end)
-conn:on("disconnection", function(conn) conn:close() end)
-conn:connect(80, "termometr.senhadri.pl")
+local sendData = function()
+    conn=net.createConnection(net.TCP, 0) 
+    conn:on("receive", function(conn, pl) print(pl) end)
+    --conn:on("sent", function(conn) numreq = numreq + 1 end)
+    conn:on("connection", function(conn) conn:send("GET /temperaturaget?temperature="..temp.." HTTP/1.1\r\nHost: termometr.senhadri.pl\r\n"
+        .."Connection: close\r\nAccept: */*\r\n\r\n") end)
+    conn:on("disconnection", function(conn) conn:close() end)
+    conn:connect(80, "termometr.senhadri.pl")
+end
+tmr.alarm(1, 1000, 1, readData)
+tmr.alarm(0, 3000, 1, sendData)
