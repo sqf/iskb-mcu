567e3fd705300438c87df7812684c14e4876c4fb
Samir Senhadri <sqf@o2.pl>
2015-12-03 19:23:28 +0100

    Sending movement data in interrupt.

--- sendingPost.lua
+++ sendingPost.lua
@@ -13,27 +13,25 @@ local readData = function()
     status,temp,humi,temp_decimial,humi_decimial = dht.readxx(gpio14)
     if( status == dht.OK ) then
         status = "OK"
-        print(string.len(status))
         print("DHT Temperature:"..temp..";".."Humidity:"..humi)
     elseif( status == dht.ERROR_CHECKSUM ) then
         status = "DHT Checksum error."
         print( "DHT Checksum error." );
     elseif( status == dht.ERROR_TIMEOUT ) then
+        temp = 0
+        humi = 0
         status = "DHT Time out."
         print( "DHT Time out." );
     end
-    --motionSensor = gpio.int(gpio12)
-    print(motionSensor)
 end
 
 local sendTempAndHumi = function()
     local key = 42
-    local dataLength = string.len(temp..humi..placeName..status) + key
-    print(dataLength)
+    local contentLength = string.len(temp..humi..placeName..status) + key
     conn = net.createConnection(net.TCP, 0) 
     conn:on("receive", function(conn, pl) print(pl) end)
     conn:on("connection", function(conn) conn:send("POST /sensor1 HTTP/1.1\r\nHost: iskb.senhadri.pl\r\n"
-        .."Content-Type: application/x-www-form-urlencoded\r\nContent-Length: "..dataLength.."\r\n\r\ntemperature="..temp.."&humidity="..humi.."&place_name="..placeName
+        .."Content-Type: application/x-www-form-urlencoded\r\nContent-Length: "..contentLength.."\r\n\r\ntemperature="..temp.."&humidity="..humi.."&place_name="..placeName
         .."&status="..status.."\r\n\r\n") end)
     conn:on("disconnection", function(conn) conn:close() end)
     conn:connect(80, "iskb.senhadri.pl")
@@ -41,14 +39,16 @@ end
 
 local sendMovement = function()
     print("movement!!!")
+    local key = 11
+    local contentLength = string.len(placeName) + key
     conn = net.createConnection(net.TCP, 0) 
     conn:on("receive", function(conn, pl) print(pl) end)
-    conn:on("connection", function(conn) conn:send("POST /sensor1 HTTP/1.1\r\nHost: iskb.senhadri.pl\r\n"
-        .."Content-Type: application/x-www-form-urlencoded\r\nContent-Length: 16\r\n\r\ntemperature="..motionSensor.."\r\n\r\n") end)
+    conn:on("connection", function(conn) conn:send("POST /movement1 HTTP/1.1\r\nHost: iskb.senhadri.pl\r\n"
+        .."Content-Type: application/x-www-form-urlencoded\r\nContent-Length: "..contentLength.."\r\n\r\nplace_name="..placeName.."\r\n\r\n") end)
     conn:on("disconnection", function(conn) conn:close() end)
     conn:connect(80, "iskb.senhadri.pl")
 end
 
---gpio.trig(gpio12, "up", sendMovement)
+gpio.trig(gpio12, "up", sendMovement)
 tmr.alarm(1, 3000, 1, readData)
 tmr.alarm(0, 3000, 1, sendTempAndHumi)
