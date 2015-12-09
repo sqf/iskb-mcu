6a051975d83e24829836be23bc26ba6480a51598
Samir Senhadri <sqf@o2.pl>
2015-12-09 16:21:30 +0100

    Deleted semicolons.

--- init.lua
+++ init.lua
@@ -1,46 +1,48 @@
-wifi.setmode(wifi.STATION);
-require('configuration');
-wifi.sta.config(ssid, pass);
-local gpio14 = 5;
-local gpio12 = 6;
-gpio.mode(gpio12, gpio.INT);
-print('aaaaaaaaaa3');
-print(wifi.sta.status());
+wifi.setmode(wifi.STATION)
+require('configuration')
+wifi.sta.config(ssid, pass)
+local gpio14 = 5
+local gpio12 = 6
+gpio.mode(gpio12, gpio.INT)
+print('aaaaaaaaaa4')
+print(wifi.sta.status())
 
 local function readTempAndHumi() 
-    local status, temp, humi = dht.readxx(gpio14)
-    local measurements = {temperature, humidity, status}
+    local status, temp, humi = dht.read(gpio14)
+    local measurements = {}
     if (status == dht.OK) then
-        measurements.temperature = temp;
-        measurements.humidity = humi;
-        measurements.status = "OK";
-        print("DHT Temperature:"..temp..";".."Humidity:"..humi);
-        return measurements;
+        measurements.temperature = temp
+        measurements.humidity = humi
+        measurements.status = "OK"
+        print("DHT Temperature:"..temp.."".."Humidity:"..humi)
+        return measurements
     elseif (status == dht.ERROR_CHECKSUM) then
-        measurements.temperature = 0;
-        measurements.humidity = 0;
-        measurements.status = "Sensor checksum error.";
-        print("Sensor checksum error.");
-        return measurements;
+        measurements.temperature = 0
+        measurements.humidity = 0
+        measurements.status = "Sensor checksum error."
+        print("Sensor checksum error.")
+        return measurements
     elseif (status == dht.ERROR_TIMEOUT) then
-        measurements.temperature = 0;
-        measurements.humidity = 0;
-        measurements.status = "Sensor time out.";
-        print("Sensor time out.");
-        return measurements;
+        measurements.temperature = 0
+        measurements.humidity = 0
+        measurements.status = "Sensor time out."
+        print("Sensor time out.")
+        return measurements
     end
 end
 
 local function generatePostMessage(requestUri, host, keysAndValuesTable)
     local keysAndValues = "";
     for key, value in pairs(keysAndValuesTable) do
-        keysAndValues = keysAndValues..key.."="..value.."&"
+        keysAndValues = keysAndValues..key.."="..value.."&";
     end
-    keysAndValues = string.sub(keysAndValues, 1, string.len(keysAndValues) - 1); -- deleting "&" at end
+    keysAndValues = string.sub(keysAndValues,
+        1, string.len(keysAndValues) - 1); -- deleting "&" at end
     local contentLength = string.len(keysAndValues);
-    return "POST "..requestUri.." HTTP/1.1\r\nHost: "..host.."\r\n"
-            .."Content-Type: application/x-www-form-urlencoded\r\nContent-Length: "
-            ..contentLength.."\r\n\r\n"..keysAndValues
+    return "POST "..requestUri.." HTTP/1.1\r\nHost: "..host.."\r\n"..
+           "Content-Type: application/x-www-form-urlencoded\r\n"..
+           "Content-Length: "..contentLength..
+           "\r\n\r\n"..keysAndValues
 end
 
 local function makeRequest(host, message)
@@ -56,18 +58,18 @@ local function makeRequest(host, message)
 end
 
 local function readAndThenSendTempAndHumi()
-    local measurements = readTempAndHumi();
-    measurements.place_name = placeName;
+    local measurements = readTempAndHumi()
+    measurements.place_name = placeName
     local postMessage = generatePostMessage("/measurement",
-        targetHost, measurements);
-    makeRequest(targetHost, postMessage);
+        targetHost, measurements)
+    makeRequest(targetHost, postMessage)
 end
 
 local function sendMovement()
-    local movement = {place_name = placeName};
-    local postMessage = generatePostMessage("/movement", targetHost, movement);
-    makeRequest(targetHost, postMessage);
+    local movement = {place_name = placeName}
+    local postMessage = generatePostMessage("/movement", targetHost, movement)
+    makeRequest(targetHost, postMessage)
 end
 
-gpio.trig(gpio12, "up", sendMovement);
-tmr.alarm(1, interval, 1, readAndThenSendTempAndHumi);
+gpio.trig(gpio12, "up", sendMovement)
+tmr.alarm(1, interval, 1, readAndThenSendTempAndHumi)
